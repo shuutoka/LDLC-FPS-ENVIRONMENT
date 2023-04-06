cfc1a35517736efb1c25963f4573691541d98e7b
shuutoka <ggchoutca@gmail.com>
2023-04-06 16:20:26 +0200

    mise à jour de la ptn de lave (╯°□°）╯︵ ┻━┻ et de la place avec plus de collider pour les dalles des arbres

--- Assets/cubelave.cs
+++ Assets/cubelave.cs
@@ -14,7 +14,7 @@ public class cubelave : MonoBehaviour
     {
         Vector3 currentPosition = transform.position;
         Vector3 direction = new Vector3(0f, 1f, 0f);
-        float speed = 0.01f;
+        float speed = 0.02f;
 
         Vector3 newPosition = currentPosition + direction * speed * Time.deltaTime;
 

af31d2b358166bb8c377deff6b1cd5b9f37c014e
abaument <>
2023-04-07 12:56:55 +0200

    DEATH COUNTERRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR

--- Assets/cubelave.cs
+++ Assets/cubelave.cs
@@ -1,21 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class cubelave : MonoBehaviour
 {
+    public TMP_Text Mort; 
+        int morts;
+
     Rigidbody rigidbody;
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
         lastKnownPosition = transform.position;
-
+        morts = 0;
+        Mort.text = "Death Counter : " + morts; 
     }
    public Vector3 lastKnownPosition;
 
     public void ResetPosition()
     {
         transform.position = lastKnownPosition;
+        morts ++; 
+        Mort.text = "Death Counter = " + morts;
     }
 
     public void SavePosition()
